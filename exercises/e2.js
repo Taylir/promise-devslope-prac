f15f28a34188a94e66c5697beb71a22ddd4e897a
Taylir <tdl9716@gmail.com>
2023-07-08 17:41:42 -0400

    Finish First Two Exercises

--- exercises/e2.js
+++ exercises/e2.js
@@ -15,8 +15,8 @@
  * Example: export const getPromise = (bool) = return <Your promise constructor code>
  */
 
-export const getPromise = () => {
-  // Your code goes here...
+export const getPromise = (bool) => {
+  return new Promise((res, rej) => bool ? res(`The PROMISE was RESOLVED`) : rej(`The PROMISE was REJECTED`))
 };
 
 /**
@@ -28,9 +28,7 @@ export const getPromise = () => {
  * The handlePromise() function must be exported
  */
 
-export const handlePromise = () => {
-  // Your code goes here...
-};
+export const handlePromise = (promise) => promise.then((data => data), (_ => "Uh Oh"));
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
