47e5f9cc72982da4ef8a8f0234c3a6c6206beb3e
Taylir <tdl9716@gmail.com>
2023-09-05 13:01:08 -0400

    Finish EX 10 and 11

--- exercises/e10.js
+++ exercises/e10.js
@@ -24,7 +24,7 @@ export const promiseArr = [promise1, promise2, promise3, promise4];
  */
 
 // Your code goes here...
-export const handlePromise1 = Promise.any(promiseArr).catch((e) => e);
+export const handlePromise1 = Promise.all(promiseArr).catch((e) => e);
 
 /**
  * @task
@@ -41,7 +41,9 @@ export const handlePromise1 = Promise.any(promiseArr).catch((e) => e);
  */
 
 // Your code goes here...
-
+  export const handlePromise2 = arr => {
+    return Promise.any(arr).then(value => value);
+  }
 /**
  * @task
  * * Create the handlePromise3 function that follows:
@@ -57,7 +59,10 @@ export const handlePromise1 = Promise.any(promiseArr).catch((e) => e);
  */
 
 // Your code goes here...
-
+export const handlePromise3 = (arr) => {
+  return Promise.allSettled(arr).then(values => 
+    values.map(value => value))
+}
 /**
  * @task
  * Update the filter method callback to filter out any promise that will be settled before promise4
@@ -66,7 +71,11 @@ export const handlePromise1 = Promise.any(promiseArr).catch((e) => e);
  * The value of newPromiseArr MUST have more than one promise in the array!
  */
 
-export const newPromiseArr = promiseArr.filter(/* <Your code goes here>*/);
+export const newPromiseArr = promiseArr.filter(res => res.then(data => {
+  if (data == 'RESOLVED AGAIN') {
+    return data
+  }
+}).catch(err => err));
 
 // Do NOT refactor or update handlePromise4 function, it's all set to work
 export const handlePromise4 = (arr) => {
--- exercises/e11.js
+++ exercises/e11.js
@@ -13,6 +13,7 @@
  * * set the usersUrl constant to store the json-server 'users' endpoint path
 */
 
+import fetch from "node-fetch";
 export const usersUrl = 'http://localhost:3000/users/';
 
 /**
@@ -26,10 +27,7 @@ export const usersUrl = 'http://localhost:3000/users/';
  * Example: const getLoginList = (data) => {<Your code>}
 */
 
-const getLoginList = () => {
-  // Your code goes here...
-
-}
+const getLoginList = (data) => data.map(user => user.login);
 
 /**
  * @task 
@@ -39,7 +37,7 @@ const getLoginList = () => {
 */
 
 // Your code goes here ...
-const getData;
+const getData = fetch(usersUrl);
 
 /**
  * @task 
@@ -53,7 +51,13 @@ const getData;
 */
 
 // Your code goes here ...
-export const result = getData;
+export const result = getData
+  .then(arr => arr.json())
+  .then(data => {
+    let users = getLoginList(data);
+    console.log(users);
+    return users;
+  });
 
 
 // === TEST YOURSELF ===
